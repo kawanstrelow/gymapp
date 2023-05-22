11f8f5518f3d708ce39d389fc236ca636c28256e
kawanstrelowm <kawanstrelow1@gmail.com>
2023-05-22 19:33:20 -0400

    Added font-family heading for all Heading components

--- src/components/ExerciseCard.tsx
+++ src/components/ExerciseCard.tsx
@@ -20,7 +20,7 @@ export function ExerciseCard({...rest} : Props) {
                     resizeMode='cover'
                 />
                 <VStack flex={1}>
-                    <Heading fontSize='lg' color='white'>Remana Unilateral</Heading>
+                    <Heading fontSize='lg' color='white' fontFamily="heading">Remana Unilateral</Heading>
                     <Text fontSize='sm' color='gray.200' mt={1} numberOfLines={2}>3 séries x 12 repetições</Text>
                 </VStack>
                 
